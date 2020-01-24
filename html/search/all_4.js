74e67a0039f22b21d0feee516c74c4aea92e7769
Travis CI <travis@travis-ci.org>
2020-01-24 15:40:30 +0000

    Deploy code docs to GitHub Pages Travis build: 23
    Commit: 09c14dc24a3221b4ff1a5a11aa08f394166fbaf5

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['get_5fversion',['get_version',['../namespacearduino__util.html#a1b4d2c8304d94c4deadb6c7eaaba998b',1,'arduino_util::get_version()'],['../bit__op_8h.html#a3c2e2d512654fcb6d990fa0dc76b41ad',1,'arduino_util::bit_op::get_version()'],['../millis__delay_8h.html#a31e938b56f0b958dedfd6be3a3690c80',1,'arduino_util::millis_delay::get_version()']]]
+  ['getversion',['getVersion',['../namespacearduino__util.html#a2f8203c5c805732bbd5cfc5aec6c33c0',1,'arduino_util::getVersion()'],['../bit__op_8h.html#a41f2c9fd4c8d3f52197d1d324a4a8271',1,'arduino_util::bit_op::getVersion()'],['../millis__delay_8h.html#ae95ef5010eb1774dc081445417d1b872',1,'arduino_util::millis_delay::getVersion()']]]
 ];
